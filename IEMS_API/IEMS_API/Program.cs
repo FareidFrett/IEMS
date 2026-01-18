bab1ff90310e6b34a12b64ff7a007d447ed007dc
Fareid Ramadan <fareidfrettchen@hotmail.com>
2026-01-18 20:58:07 +0100

    SCRUM-7 Create Backend Project

---
+++ IEMS_API/IEMS_API/Program.cs
@@ -0,0 +1,37 @@
+
+namespace IEMS_API;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+            // Adds the Swagger UI Dashboard
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/openapi/v1.json", "v1");
+            });
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
